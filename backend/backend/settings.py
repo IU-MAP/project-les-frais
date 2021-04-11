051a81a704f5bdd097b32a4e88eef85ea94a9831
Andrey862 <F_A_M@list.ru>
2021-04-11 17:21:46 +0300

    changed DEFAULT_AUTHENTICATION_CLASSES to session and token instead of basic and session

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -137,3 +137,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    # other settings...
+    'DEFAULT_AUTHENTICATION_CLASSES': [ 
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication'],
+}
