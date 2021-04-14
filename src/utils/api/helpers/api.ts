a59c2e76ebcd955cd971d8395ebf3cc148087ed7
matveyplevako <matveyplevako@rambler.ru>
2021-04-14 14:02:22 +0300

    serve csrf token with header

--- src/utils/api/helpers/api.ts
+++ src/utils/api/helpers/api.ts
@@ -2,10 +2,24 @@
 import { API_URL } from '../../constants';
 import ApiError from './api-error';
 import { store } from '../../../store';
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+
+var csrftoken = readCookie('csrftoken');
+
 
 const http = async <T>(path: string, config: RequestInit): Promise<T> => {
   const headers: HeadersInit = {
     'Content-Type': 'application/json',
+"X-CSRFToken": csrftoken,
   };
   if (store?.state?.token) headers.Authorization = `Token ${store.state.token}`;
 
