054d73b3d380d27a911b6d17e5a9e34a51dfa0e6
matveyplevako <matveyplevako@rambler.ru>
2021-05-05 13:45:39 +0300

    patch fix

--- src/utils/api/helpers/api.ts
+++ src/utils/api/helpers/api.ts
@@ -87,7 +87,7 @@ const request = {
    * PATCH-request. Use it to update entity instances
    */
   patch: <T, U>(path: string, body: T, config?: RequestInit): Promise<U> => {
-    const init = { method: 'patch', body: JSON.stringify(body), ...config };
+    const init = { method: 'PATCH', body: JSON.stringify(body), ...config };
     return http<U>(path, init);
   },
 
