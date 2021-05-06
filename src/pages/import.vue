819e333925e974c56c2e60015d7637b3eb999929
VanishMax <maxkors2014@gmail.com>
2021-05-06 22:19:12 +0300

    Add import page base

---
+++ src/pages/import.vue
@@ -0,0 +1,130 @@
+<template>
+  <section class="import-page">
+    <nav class="import-navigation">
+      <Button :link="{ name: 'settings', query: { slug: 'settings_tabs_profile' }}">
+        <BackIcon />
+      </Button>
+      <Button look="with-icon">
+        {{ t('import_update_file') }}
+        <FileIcon />
+      </Button>
+      <h1>{{ t('import_h1') }}</h1>
+      <Button look="submit">{{ t('import_import') }}</Button>
+    </nav>
+
+    <p class="import-info text-regular">{{ t('import_info') }}</p>
+    <p class="import-error text-regular text-color-error">{{ error }}</p>
+
+    <div class="import-table">
+      <div v-for="(header, i) in HEADERS" :key="i">
+        <div>
+          <h3>{{ header.name }}</h3>
+          <XIcon />
+        </div>
+        <Dropdown :items="COLUMNS" @select="chooseColumn(header.name, $event)">
+          <template #default>
+            {{ header.chosen ? t(header.chosen.name) : t('import_choose_field') }}
+          </template>
+          <template #option="{item}">
+            {{ t(item.name) }}
+          </template>
+        </Dropdown>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import '../assets/styles/pages/import.css';
+import {
+  computed, defineComponent, ref,
+} from 'vue';
+import useTranslation from '../utils/useTranslation';
+import Button from '../components/button/index.vue';
+import BackIcon from '../assets/icons/back.svg';
+import FileIcon from '../assets/icons/file.svg';
+import XIcon from '../assets/icons/x.svg';
+import Dropdown from '../components/dropdown/index.vue';
+
+const HEADERS: { name: string, chosen: { slug: string, name: string }|null }[] = [
+  {
+    name: 'Title',
+    chosen: null,
+  },
+  {
+    name: 'Cat',
+    chosen: null,
+  },
+  {
+    name: 'Date',
+    chosen: null,
+  },
+  {
+    name: 'Time',
+    chosen: null,
+  },
+  {
+    name: 'Sum',
+    chosen: null,
+  },
+];
+
+const ROWS = [
+  ['Row', '', '2020-03-14', '16:14:11', '5420'],
+  ['New iPhone', 'tech', '2020-03-14', '', '86000'],
+  ['Dinner', 'Food', '2020-03-15', '20:17:18', '86000'],
+];
+
+const COLUMNS = [
+  {
+    slug: 'date',
+    name: 'import_date', // Is taken from the translations like t('import_date')
+  },
+  {
+    slug: 'name',
+    name: 'import_name',
+  },
+  {
+    slug: 'category',
+    name: 'import_category',
+  },
+  {
+    slug: 'price',
+    name: 'import_price',
+  },
+];
+
+export default defineComponent({
+  name: 'ImportPage',
+  components: {
+    Dropdown,
+    Button,
+    BackIcon,
+    FileIcon,
+    XIcon,
+  },
+  setup () {
+    const t = useTranslation();
+    const error = ref<string>('Some error here');
+
+    const styles = computed<number>(() => ({
+      width: HEADERS.length,
+    }));
+
+    const chooseColumn = (header: string, column: { slug: string, name: string }) => {
+      const found = HEADERS.find((item) => item.name === header);
+      found.chosen = column;
+    };
+
+    return {
+      t,
+      error,
+      styles,
+      HEADERS,
+      ROWS,
+      COLUMNS,
+      chooseColumn,
+    };
+  },
+});
+</script>
